4aba889a9af40bdc1615c717f27bab9d177c4a23
kevinfang2 <kevkev0123@gmail.com>
2017-04-01 23:57:02 -0700

    asdf

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def max_pool_2x2(x):
 	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 def get_image(image_path):
-    return transform(imread(image_path))
+	return transform(imread(image_path))
 
 def transform(image, npx=512, is_crop=True):
     cropped_image = cv2.resize(image, (64,64))
@@ -47,20 +47,15 @@ def save(checkpoint_dir, step):
 
 sess = tf.Session()
 
-x2 = tf.placeholder(tf.float32, shape=[64,64,3])
-y2_ = tf.placeholder(tf.float32, shape=[30,3])
-
-x = tf.placeholder(tf.float32, shape=[30, 64, 64, 3])
-y_ = tf.placeholder(tf.float32, shape=[30, 3])
+x = tf.placeholder(tf.float32, shape=[None, 64, 64, 3])
+y_ = tf.placeholder(tf.float32, shape=[9, 3])
 
 W_conv1 = weight_variable([5, 5, 3, 64])
 b_conv1 = bias_variable([64])
 
-# x_image = tf.reshape(x, [-1,64,32,1])
 
 h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
-# 32 x 32 x 64
 
 
 W_conv2 = weight_variable([5, 5, 64, 64])
@@ -102,7 +97,7 @@ sn = ["nevus","atypical","melonoma"]
 batch = np.zeros((si*6,1024))
 labels = np.zeros((si*6,si))
 
-batchsize = 30
+batchsize = 9
 
 saver = tf.train.Saver()
 
@@ -116,41 +111,41 @@ if sys.argv[1] == "train":
 	baseMelonomaPath = "PH2Dataset/melonoma/melonoma"
 	baseNevusPath = "PH2Dataset/nevus/nevus"
 
-	for x in range(1,46):
-		path = baseAtypicalPath + str(x) + "/picture.bmp"
-		dataAtypical.extend(misc.imread(path))
+	for a in range(1,44):
+		path = baseAtypicalPath + str(a) + "/picture.bmp"
+		dataAtypical.append(path)
 
-		path2 = baseMelonomaPath + str(x) + "/picture.bmp"
-		dataMelonoma.extend(misc.imread(path2))
+		path2 = baseMelonomaPath + str(a) + "/picture.bmp"
+		dataMelonoma.append(path2)
 
-		path3 = baseNevusPath + str(x) + "/picture.bmp"
-		dataNevus.extend(misc.imread(path3))
+		path3 = baseNevusPath + str(a) + "/picture.bmp"
+		dataNevus.append(path3)
 
 
 	for i in range(20000):
+		batch_files = dataAtypical[i*batchsize/3:(i+1)*batchsize/3]
+		batch = np.array([get_image(batch_file) for batch_file in batch_files])
+
+		batch_files2 = dataMelonoma[i*batchsize/3:(i+1)*batchsize/3]
+		batch2 = np.array([get_image(batch_file2) for batch_file2 in batch_files2])
+
+		batch_files3 = dataNevus[i*batchsize/3:(i+1)*batchsize/3]
+		batch3 = np.array([get_image(batch_file3) for batch_file3 in batch_files3])
+
+		bigArray = np.concatenate((batch, batch2, batch3), axis=0)
+		
+		labels = np.zeros((batchsize, 3))
+		labels[0:3,0] = 1;
+		labels[3:6,1] = 1;
+		labels[6:9,2] = 1;
+
+		train_step.run(session=sess, feed_dict={x: bigArray, y_: labels, keep_prob: 0.5})
 		if i%10 == 0:
-			batch_files = dataAtypical[i*batchsize/3:(i+1)*batchsize/3]
-			print dataAtypical
-			batch = np.array([get_image(batch_file) for batch_file in batch_files])
-
-			batch_files2 = dataMelonoma[i*batchsize/3:(i+1)*batchsize/3]
-			batch2 = np.array([get_image(batch_file2) for batch_file2 in batch_files2])
-
-			batch_files3 = dataNevus[i*batchsize/3:(i+1)*batchsize/3]
-			batch3 = np.array([get_image(batch_file3) for batch_file3 in batch_files3])
-
-			labels = np.zeros((batchsize, 3))
-			labels[0:10,0] = 1;
-			labels[10:20,1] = 1;
-			labels[20:30,2] = 1;
-
-			train_accuracy = accuracy.eval(feed_dict={x:batch, y_: labels, keep_prob: 1.0})
+			train_accuracy = accuracy.eval(session=sess,feed_dict={x:bigArray, y_: labels, keep_prob: 1.0})
 			print "step %d, training accuracy %g"%(i, train_accuracy)
-            # result = y_conv.eval(feed_dict={x: batch, y_: labels, keep_prob: 1.0})
-            # for k in range(18):
-            #     print "lmao %d %s" % (k, np.array_str(result[k]))
-        if i%50 == 0:
-            saver.save(sess, "/Users/kevin/Documents/Python/facial-detection/training.ckpt", global_step=i)
+
+		if i%50 == 0:
+			saver.save(sess, "/Users/kevin/desktop/uvradiation/training/tr/training.ckpt", global_step=i)
 
         train_step.run(feed_dict={x: batch, y_: labels, keep_prob: 0.5})
 elif sys.argv[1] == "server":
@@ -174,7 +169,6 @@ def do_login():
 
     saver.restore(sess, tf.train.latest_checkpoint("/Users/kevin/Documents/Python/facial-detection/"))
     image = urllib2.urlopen('https://signatureauthentication.firebaseio.com/image.json').read()
-    image = image[1:-1]
     image = image.replace("\\r\\n", "")
 
     fh = open("imageToSave.png", "wb")
